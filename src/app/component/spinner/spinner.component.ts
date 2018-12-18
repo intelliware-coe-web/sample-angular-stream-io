dfc83aacb8e3ccb64fe33d82d308b23c0d15ef4a
mr-rampage <readonly@che.wbac.ca>
2018-12-18 01:00:27 -0500

    refactor(spinner) DRY

--- src/app/component/spinner/spinner.component.ts
+++ src/app/component/spinner/spinner.component.ts
@@ -1,12 +1,11 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
   template: `
     <div class="uk-text-center">
-      <span uk-spinner="ratio: 4.5" [hidden]="isHidden"></span>
+      <span uk-spinner="ratio: 4.5"></span>
     </div>`
 })
 export class SpinnerComponent {
-  @Input() isHidden: boolean;
 }
