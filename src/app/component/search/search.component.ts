b74d46bd7aa417d3d6c68a8e5b4b016bae4b88ad
mr-rampage <readonly@che.wbac.ca>
2018-12-18 00:36:15 -0500

    refactor(search)

--- src/app/component/search/search.component.ts
+++ src/app/component/search/search.component.ts
@@ -2,7 +2,11 @@ import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-search',
-  templateUrl: './search.component.html',
+  template: `
+    <form class="uk-search uk-search-large uk-width-1-1">
+      <span uk-search-icon></span>
+      <input class="uk-search-input" type="search" placeholder="Search..." (input)="search.emit($event.target.value)">
+    </form>`
 })
 export class SearchComponent {
   @Output()
