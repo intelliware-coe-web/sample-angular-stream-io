289812957b923b0513129d4b735f9f153e646948
mr-rampage <readonly@che.wbac.ca>
2018-12-17 20:19:15 -0500

    feat(uiKit)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+UIkit.use(Icons);
+
 if (environment.production) {
   enableProdMode();
 }
