b9625b9f5ec5e0aeb748697341f5940f18279f06
mr-rampage <readonly@che.wbac.ca>
2018-12-18 00:51:31 -0500

    feat(fetch wrapper)

---
+++ src/app/infrastructure/fetch.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {of} from 'rxjs';
+import {map, switchMap} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FetchService {
+  private static proxyRequests(url) {
+    return `http://localhost:8080/${url}`;
+  }
+
+  constructor(private http$: HttpClient) {
+  }
+
+  public request(url: string) {
+    return of(url).pipe(
+      map(href => FetchService.proxyRequests(href)),
+      switchMap(request => this.http$.get(request)),
+    );
+  }
+
+}
