d3e31b5440c77cb9a8a05b72f285b4999526448c
mr-rampage <readonly@che.wbac.ca>
2018-12-18 09:59:55 -0500

    test(marble tests)

--- src/app/effect/wikipedia-effect.service.spec.ts
+++ src/app/effect/wikipedia-effect.service.spec.ts
@@ -2,6 +2,7 @@ import {WikipediaEffectService} from './wikipedia-effect.service';
 import {FetchService} from '../infrastructure/fetch.service';
 import {TestScheduler} from 'rxjs/testing';
 import SpyObj = jasmine.SpyObj;
+import {SearchResult} from '../model/SearchResult';
 
 describe('WikipediaEffectService', () => {
   const fetchService: SpyObj<FetchService> = jasmine.createSpyObj('fetchService', ['request']);
@@ -10,12 +11,13 @@ describe('WikipediaEffectService', () => {
   it('should be created', () => {
     new TestScheduler((actual, expected) => {
       expect(actual).toEqual(expected);
-    }).run(({hot, expectObservable}) => {
-      const payload = {};
-      fetchService.request.and.returnValue(hot('a|', { a: payload}));
-      const test$ = fixture.findArticles(hot('--a--b', {a: 'url1', b: 'url2'}));
+    }).run(({cold, expectObservable}) => {
+      const payload = ['te', ['test'], ['desc'], ['link']];
+      fetchService.request.and.returnValue(cold('a|', { a: payload}));
+      const actual$ = fixture.findArticles(cold('--a', {a: 'url1'}));
+      const expected = [new SearchResult('test', 'desc', 'link')];
 
-      expectObservable(test$).toBe('--a--a', {a: payload});
+      expectObservable(actual$).toBe('-- 1000ms a', {a: expected});
     });
   });
 });
--- src/app/filter/negate.pipe.spec.ts
+++ src/app/filter/negate.pipe.spec.ts
@@ -1,8 +1,14 @@
 import { NegatePipe } from './negate.pipe';
+import {TestScheduler} from 'rxjs/testing';
 
 describe('NegatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new NegatePipe();
-    expect(pipe).toBeTruthy();
+  it('should negate an incoming stream', () => {
+    new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    }).run(({cold, expectObservable}) => {
+      const pipe = new NegatePipe();
+      const source$ = cold('--a--b', {a: true, b: false});
+      expectObservable(pipe.transform(source$)).toBe('--a--b', {a: false, b: true});
+    });
   });
 });
