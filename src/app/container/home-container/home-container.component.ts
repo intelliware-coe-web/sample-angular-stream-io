f83a0d8b3dceffcb595c7ec1c25e933c0a6df926
mr-rampage <readonly@che.wbac.ca>
2018-12-18 01:17:24 -0500

    refactor(observable) avoiding tap

--- src/app/container/home-container/home-container.component.ts
+++ src/app/container/home-container/home-container.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {WikipediaEffectService} from '../../effect/wikipedia-effect.service';
-import {tap} from 'rxjs/operators';
-import {BehaviorSubject, Observable, Subject} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {merge, Observable, Subject} from 'rxjs';
 
 @Component({
   selector: 'app-home-container',
@@ -10,19 +10,19 @@ import {BehaviorSubject, Observable, Subject} from 'rxjs';
 export class HomeContainerComponent {
   private readonly search$ = new Subject<string>();
 
-  readonly hideSpinner$ = new BehaviorSubject<boolean>(true);
+  readonly hideSpinner$: Observable<boolean>;
   readonly searchResults$: Observable<any>;
 
   constructor(private wikipediaEffect: WikipediaEffectService) {
-    this.searchResults$ = this.wikipediaEffect.findArticles(this.search$)
-      .pipe(
-        tap(() => this.hideSpinner$.next(true))
-      );
+    this.searchResults$ = this.wikipediaEffect.findArticles(this.search$);
+
+    this.hideSpinner$ = merge(
+      this.search$.pipe(map(() => false)),
+      this.searchResults$.pipe(map(() => true)),
+    ).pipe(startWith(true));
   }
 
   onSearch(search) {
-    this.hideSpinner$.next(false);
     this.search$.next(search);
   }
-
 }
